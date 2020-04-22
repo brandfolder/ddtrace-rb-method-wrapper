25c7a6f03f4dc581d268b7d6f2d9ab0e01492534
Brett Nekolny <brett.nekolny@brandfolder.com>
2020-04-22 13:04:20 -0600

    Add YARD docs.

--- lib/datadog_trace_wrapper.rb
+++ lib/datadog_trace_wrapper.rb
@@ -1,4 +1,26 @@
+# CoreUsage:
+# extend DatadogTraceWrapper
 module DatadogTraceWrapper
+
+  # Instructs the class to wrap methods for custom datadog tracing
+  #
+  # == Parameters:
+  # *method_names::
+  #   A list of method name symbols to trace
+  #   `:method1, :method2`
+  # span_type::
+  #   Keyword argument (string) specifying the name of the span
+  #
+  # == Optional Parameters:
+  # service::
+  #   String specifying the service to include this span within.
+  #   Defaults to DD_TRACE_METHOD_WRAPPER_SERVICE
+  # resource::
+  #   String specifying what to call the trace segment in the span
+  #   Defaults to ClassName#Method
+  # trace_kwargs::
+  #   Keyword arguments passed through to Datadog.tracer.trace, listed here:
+  #   https://docs.datadoghq.com/tracing/setup/ruby/#manual-instrumentation
   def trace(*method_names, span_type:, service: nil, resource: nil, **trace_kwargs)
 
     method_names.each do |m|
